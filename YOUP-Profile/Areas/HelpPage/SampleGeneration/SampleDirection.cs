9943dc8215a5ebf9b2b7d899e59a4dff9aa8585e
narkolpain <narkolapin@gmail.com>
2014-10-24 17:56:32 +0200

    [creation solution]

---
+++ YOUP-Profile/Areas/HelpPage/SampleGeneration/SampleDirection.cs
@@ -0,0 +1,11 @@
+namespace YOUP_Profile.Areas.HelpPage
+{
+    /// <summary>
+    /// Indicates whether the sample is used for request or response
+    /// </summary>
+    public enum SampleDirection
+    {
+        Request = 0,
+        Response
+    }
+}
