78c5088ac49110698e875b8cc05a43abb12bca91
peBruneval <ez.bruneval@gmail.com>
2014-11-03 19:46:56 +0100

    fix erreur check in git...

---
+++ YOUP-Profile/Controllers/AuthController.cs
@@ -0,0 +1,30 @@
+﻿using Business;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using YOUP_Profile.Models;
+using YOUP_Profile.Models.Adapters;
+namespace YOUP_Profile.Controllers
+{
+    public class AuthController : ApiController
+    {
+        private static readonly Lazy<Buisiness> lazyBuisiness =
+                new Lazy<Buisiness>(() => new Buisiness());
+        private static Buisiness Buisiness { get { return lazyBuisiness.Value; } }
+
+        /// <summary>
+        /// Authentifie un utilisateur en recevant un email et un mot de passe
+        /// </summary>
+        /// <param name="Email">Email de l'utilisateur</param>
+        /// <param name="Pass">Mot de passe non crypter de l'utilisateur</param>
+        /// <param name="Device">Optionel, permet de definir le device appelant</param>
+        /// <returns></returns>
+        public Utilisateur Post(string Email, string Pass, string Device = null)
+        {
+            return Buisiness.GetUtilisateurByEmailPassword(Email, Pass, Device).ToExpo();
+        }
+    }
+}
---
+++ YOUP-Profile/Models/Adapters/Adapters.cs
@@ -0,0 +1,42 @@
+﻿using Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace YOUP_Profile.Models.Adapters
+{
+    public static class Adapters
+    {
+        public static Utilisateur ToExpo(this UtilisateurBusiness u)
+        {
+            return new Utilisateur(u);
+        }
+
+        public static UtilisateurBusiness ToBusiness(this Utilisateur u)
+        {
+            return new UtilisateurBusiness()
+            {
+                Utilisateur_Id = u.Utilisateur_Id,
+                Pseudo = u.Pseudo,
+                MotDePasse = u.MotDePasse,
+                DateInscription = u.DateInscription,
+                Nom = u.Nom,
+                Prenom = u.Prenom,
+                Sexe = u.Sexe,
+                AdresseMail = u.AdresseMail,
+                DateNaissance = u.DateNaissance,
+                Ville = u.Ville,
+                CodePostal = u.CodePostal,
+                PhotoChemin = u.PhotoChemin,
+                Situation = u.Situation,
+                Actif = u.Actif,
+                Partenaire = u.Partenaire,
+                Presentation = u.Presentation,
+                Metier = u.Metier,
+                Amis = u.Amis,
+                Categories = u.Categories
+            };
+        }
+    }
+}
