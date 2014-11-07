a166f5eb4ab68de17f2d77352688ac7eb8ac38c7
Harold- <tabaudh@voila.fr>
2014-11-07 18:55:35 +0100

    #PRO18 invit Event Couche exploit GetInvitEvent(int,int,int)

---
+++ YOUP-Profile/Controllers/InvitEventController.cs
@@ -0,0 +1,29 @@
+﻿using Business;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace YOUP_Profile.Controllers
+{
+    public class InvitEventController : ApiController
+    {
+        private static readonly Lazy<Buisiness> lazyBuisiness =
+                new Lazy<Buisiness>(() => new Buisiness());
+        private static Buisiness Buisiness { get { return lazyBuisiness.Value; } }
+
+        public bool GetInvitEvent(int event_id, int user_id, int invit_id)
+        {
+            try { 
+            Buisiness.GetInvitEvent(event_id, user_id, invit_id);
+                return true;
+            }
+            catch{
+                return false;
+
+            }
+    }
+}
