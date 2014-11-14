82b9b27c365af6a44307302f4eace806209334ba
Harold- <tabaudh@voila.fr>
2014-11-14 17:05:35 +0100

    #PRO18 Invit event Commentaire sur les méthodes

--- YOUP-Profile/Controllers/InvitEventController.cs
+++ YOUP-Profile/Controllers/InvitEventController.cs
@@ -15,6 +15,13 @@ namespace YOUP_Profile.Controllers
                 new Lazy<Buisiness>(() => new Buisiness());
         private static Buisiness Buisiness { get { return lazyBuisiness.Value; } }
 
+        /// <summary>
+        /// Envoie une invitation à un événement d'un utilisateur à un autre
+        /// </summary>
+        /// <param name="event_id"> Id de l'évènement</param>
+        /// <param name="invit_id"> Id de l'utilisateur invité</param>
+        /// <param name="user_id"> Id du créateur de l'event</param>
+        /// <returns>true si l'invit OK False si l'invit KO</returns>
         public bool GetInvitEvent(int event_id, int user_id, int invit_id)
         {
             try
